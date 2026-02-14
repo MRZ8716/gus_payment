699ea19f1489dd9ab977b9333d27896721ef33d5
Nizam MRZ <nizam87.mrz@gmail.com>
2026-02-14 16:39:36 +0700

    feat : add csp

---
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Vite;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContentSecurityPolicy
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        Vite::useCspNonce();
+        $nonce = Vite::cspNonce();
+
+        $styleSrc = "";
+        $scriptSrc = "";
+        $styleNonce = "nonce-" . $nonce;
+        $scriptNonce = "nonce-" . $nonce;
+
+        $routeName = $request->route()?->getName();
+
+        $unsafeInlineRoutes = [
+            'welcome',
+            'landing',
+            'home',
+        ];
+
+        if (in_array($routeName, $unsafeInlineRoutes, true)) {
+            $styleSrc  = " 'unsafe-inline'";
+            $scriptSrc = " 'unsafe-inline'";
+        }
+
+        $csp = "
+            default-src 'self';
+            script-src 'self' " . $scriptSrc . " " . $scriptNonce . ";
+            style-src 'self' " . $styleSrc . " " . $styleNonce . ";
+            img-src 'self' data:;
+            font-src 'self';
+            frame-src 'none';
+            object-src 'none';
+            base-uri 'self';
+            form-action 'self';
+        ";
+
+        $response->headers->set(
+            'Content-Security-Policy',
+            preg_replace('/\s+/', ' ', trim($csp))
+        );
+
+        return $response;
+    }
+}
